740b8ff52736f1af4167a8786462c3786ecef90a
singe <singe-github@singe.za.net>
2020-03-18 16:24:30 +0200

    @scriptjunkie1 showed me my rust errors and made it fast

--- permute.rs
+++ permute.rs
@@ -1,32 +1,34 @@
-use std::io::{self, Write, BufReader};
-use std::io::BufRead;
+/*
+  A rust implementation of hashcat-util's permute.c
+  https://github.com/hashcat/hashcat-utils/blob/master/src/permute.c
+
+  By @singe with huge help from @scriptjunkie1
+
+  Usage: ./permute <file>
+*/
+
+
+use std::io::{self, Write, BufReader, BufRead};
 use std::fs::File;
 use std::env;
 
-struct OutT {
-  fp: std::io::Stdout,
-  buf: String,
-  len: usize,
+// Write our buffer to stdout
+fn out_flush(out: &mut Vec<u8>, handle: &mut std::io::StdoutLock) {
+  //if out.len() == 0 { return(); }
+  handle.write(&out).unwrap();
+  out.clear();
 }
 
-fn out_flush(out: &mut OutT) {
-  if out.len == 0 { return(); }
-  let mut handle: std::io::StdoutLock = out.fp.lock();
-  handle.write(out.buf.as_bytes()).unwrap();
-  out.len = 0;
-}
-
-fn out_push(out: &mut OutT, pw_buf: Vec<u8>, pw_len: usize) {
-  let strbuf = String::from_utf8(pw_buf).unwrap();
-  out.buf.push_str(&strbuf);
-  out.buf.push_str("\n");
-  out.len += pw_len + 1;
-
-  if out.len >= 8192 - 300 {
-    out_flush(out);
+// Push a newly generated permutation to our buffer
+// if we hit the threshold flush it to stdout
+fn out_push(out: &mut Vec<u8>, pw_buf: &[u8], handle: &mut std::io::StdoutLock) {
+  out.extend_from_slice(&pw_buf);
+  if out.len() >= 8192 - 300 {
+    out_flush(out, handle);
   }
 }
 
+// Generate the next permutation using the quickperm algorithm
 fn next_permutation(word: &mut Vec<u8>,p: &mut Vec<usize>, mut k: usize) -> usize {
   p[k] -= 1;
   let j = k % 2 * p[k];
@@ -40,20 +42,23 @@ fn next_permutation(word: &mut Vec<u8>,p: &mut Vec<usize>, mut k: usize) -> usiz
 }
 
 fn main() {
+  let so = io::stdout();
+  let mut handle: std::io::StdoutLock = so.lock();
+
+  // Open the filename passed in
   let args: Vec<String> = env::args().collect();
   let f = File::open(&args[1]).unwrap();
   let f = BufReader::new(f);
 
-  for line in f.lines() {
-    let mut line_buf = line.unwrap().into_bytes();
-    let line_len = line_buf.len();
+  let mut out = Vec::with_capacity(8192);
 
-    let mut out = OutT {
-      fp: io::stdout(),
-      buf: String::from(""),
-      len: 0,
-    };
-    
+  for line in f.lines() {
+    let mut line_str = line.unwrap();
+    line_str.push_str("\n");
+    let mut line_buf = line_str.into_bytes();
+    let line_len = line_buf.len() - 1;
+
+    // Initialise our quickperm vector 
     let mut k;  
     let mut p = vec![0; line_len+1];
     for k in 0..(line_len+1) {
@@ -61,12 +66,14 @@ fn main() {
     }
     k = 1;
 
-    out_push(&mut out, line_buf.clone(), line_len);
+    // Generate permutations and put them in the buffer
+    out_push(&mut out, &line_buf, &mut handle);
     while k < line_len {
       k = next_permutation(&mut line_buf, &mut p, k);
-      out_push(&mut out, line_buf.clone(), line_len);
+      out_push(&mut out, &line_buf, &mut handle);
     }
-    //out_push(&mut out, &line_buf, line_len);
-    out_flush(&mut out);
-  } 
+
+    // We're done do a flush of anything left in the buffer
+    out_flush(&mut out, &mut handle);
+  } // fetch the next line from the file
 }
